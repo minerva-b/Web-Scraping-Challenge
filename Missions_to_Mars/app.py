30374cccf75cd42d7daab986bc108ca011b5d8e0
minerva-b <64626133+minerva-b@users.noreply.github.com>
2020-08-22 17:11:05 -0700

    Adding templates folder; moving index.html, mars_facts.html; pushing scrape_mars.py, app.py

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,38 @@
+# Import dependencies:
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Create an instance of Flask:
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection:
+mongo = PyMongo(app, uri="mongodb://localhost:27017/misson_mars_app")
+
+
+# Route to render index.html template using data from Mongo:
+@app.route("/")
+def home():
+
+    # Find the existing mars collection from the mongo database, if not it will create one:
+    mission_data = mongo.db.collection.find_one()
+
+    # Return template and data. Set the variable mars equal to the data from the database
+    return render_template("index.html", mars=mission_data)
+
+# Route that will trigger the scrape function:
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function:
+    mars_data = scrape_mars.scrape()
+
+    # Update the Mongo database using update and upsert=True:
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page:
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,130 @@
+# Importing dependencies:
+import pandas as pd
+from splinter import Browser
+from splinter.exceptions import ElementDoesNotExist
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+import time
+
+
+def init_browser():
+    executable_path = {"executable_path": "C://Users/mgban/Desktop/chromedriver.exe"}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape_info():
+    browser = init_browser()
+
+    # Visit NASA url:
+    nasa_url = "https://mars.nasa.gov/news/"
+    browser.visit(nasa_url)
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time = 1)
+
+    # Scrape page into NASA Soup:
+    html = browser.html
+    nasa_soup = bs(html, "html.parser")
+
+    # Scraping and collecting the lastest 'News Title' and 'Paragraph Text':
+    # latest_news_article = nasa_soup.find('ul', class_='item_list')
+    news_title = nasa_soup.find('div', class_='content_title').get_text()
+    news_parag = nasa_soup.find('div', class_='article_teaser_body').get_text()
+
+    #-------------------------------------------------------------------------------------
+
+    # Visit JPL url:
+    jpl_url  = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(jpl_url )
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time = 1)
+
+    # Scrape page into JPL Soup:
+    html = browser.html
+    jpl_soup = bs(html, "html.parser")
+    
+    # Scraping and collecting JPL's featured image:
+    featured_image_path = jpl_soup.find('a', class_='button fancybox').get('data-fancybox-href').strip()
+    featured_image_url = jpl_url + featured_image_path
+
+    #-------------------------------------------------------------------------------------
+
+    # Visit Space Facts url:
+    space_facts_url  = "https://space-facts.com/mars/"
+    browser.visit(space_facts_url )
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time = 1)
+
+    # Scrape page into Space Facts Soup:
+    html = browser.html
+    space_soup = bs(html, "html.parser")
+
+    # Using pandas we will scrape the webpage to read the table of facts:
+    mars_facts = pd.read_html(space_facts_url)
+    
+    # Printing only the first table from the webpage
+    df = mars_facts[0]
+
+    # Renaming the columns:
+    mars_facts_df = df.rename(columns={0: "", 1: "Mars"})
+
+    # Using pandas, generate HTML table from Dataframe:
+    html_table = mars_facts_df.to_html()
+
+    # Strip unwanted newlines to clean up the table:
+    html_table.replace('\n', '')
+
+    # Saving the table directly to a file:
+    mars_table = mars_facts_df.to_html('mars_facts.html', index=False)
+
+    #-------------------------------------------------------------------------------------
+
+    # Visit USGS url:
+    usgs_url = "https://astrogeology.usgs.gov/"
+
+    # Since we must go into each hemisphere's link to gather the appropiate image_url
+    # Loop and iterate through all four hemisphere's link:
+    urls = [
+        'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced',
+        'https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced',
+        'https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced',
+        'https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced'
+    ]
+
+    # Scraping for image 'title' and image 'url' for all hemispheres:
+    img_url_list = []
+
+    # To search through each link and scrape the image 'title' and 'img_url':
+    for url in urls:
+        # Creating a path to the 'chromedriver' to be able to use 'Browser' from splinter:
+        executable_path = {'executable_path':'C://Users/mgban/Desktop/chromedriver.exe'}
+        browser = Browser('chrome', **executable_path, headless=False)
+        browser.visit(url)
+        browser.is_element_present_by_css("ul.item_list li.slide", wait_time = 1)
+        
+        # HTML object:
+        html = browser.html
+        # Parse HTML with BeautifulSoup as bs 'html.parser' or 'lxml':
+        soup = bs(html, 'html.parser')
+        
+        # Searching for through the tags:
+        title = soup.find('h2', class_='title').text
+        img_url = soup.find('img', class_='wide-image')['src']
+        
+        # Storing the 'title' and 'img_url':
+        img_url_list.append({
+            'Image Title': title, 
+            'Image URL': usgs_url + img_url
+        })
+
+    # Creating Python Dictionary:
+    scraped_data = {
+        'News_Title': news_title,
+        'News_Pg': news_parag,
+        'Featured_Img_URL': featured_image_url,
+        'Mars_Facts_Table': mars_table,
+        'Image_URL_List': img_url_list,
+    }
+
+    # Close the browser after scraping:
+    browser.quit()
+
+    # Return results:
+    return mars_data
